13af2754485eb1e4e7183a589bcf14e450ca84fa
cheroliv <cheroliv@users.noreply.github.com>
2026-06-19 21:55:39 +0200

    Fix POM developer email — ccp.education@gmail.com → ccp.edu@gmail.com

--- newpipe-plugin/newpipe/build.gradle.kts
+++ newpipe-plugin/newpipe/build.gradle.kts
@@ -110,7 +110,7 @@ publishing {
                         developer {
                             id.set("cccp-education")
                             name.set("CCCP Education")
-                            email.set("cccp.education@gmail.com")
+                            email.set("cccp.edu@gmail.com")
                         }
                     }
                     scm {
